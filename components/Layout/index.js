26a51118d30c10e3b977c12f6d5d6d3994a7f309
Gavin NUS <gavin@nustechnology.com>
2020-11-16 16:15:18 +0700

    Install Newrelic browser monitoring

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head"
 
 import Header from "../Header"
+import { newrelicSPAMonitoring } from "./newrelic_spa_monitoring"
 
 export default function Layout({ children }) {
   return (
     <>
       <Head>
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: newrelicSPAMonitoring}} />
         <title>Bikebiz Replatform</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
