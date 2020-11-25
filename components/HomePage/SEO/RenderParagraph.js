ab5fc1e1e3d2ae11a325543cc33e0185222e2257
LeNH NUS <lenh@nustechnology.com>
2020-11-25 17:39:14 +0700

    BBSFR-37: path html from text

--- components/HomePage/SEO/RenderParagraph.js
+++ components/HomePage/SEO/RenderParagraph.js
@@ -2,20 +2,24 @@ import React from "react"
 import styles from "../HomePage.module.scss"
 
 export default function renderParagraph({ pageParagraph, showMore, detect }) {
+  let s
   return (
     <div className={styles.page_paragraph}>
       <div className={styles.page_paragraph}>
         {pageParagraph.map((paragraph, id) =>
           showMore ? (
-            <p>
-              {paragraph.text.link("facebook.com")}
-              {paragraph.text}
-            </p>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: paragraph.text.link("facebook.com"),
+              }}
+            ></p>
           ) : id <= detect.position ? (
             id < detect.position ? (
-              <p>{paragraph.text}</p>
+              <p>{paragraph.text.link("facebook.com")}</p>
             ) : (
-              <p>{`${paragraph.text.substring(0, detect.substring - 1)}...`}</p>
+              <p>{`${paragraph.text
+                .substring(0, detect.substring - 1)
+                .link("facebook.com")}...`}</p>
             )
           ) : (
             ""
