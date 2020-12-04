5264ccde86a769e0d1291e7f72a0ed6dc45c1180
LeNH NUS <lenh@nustechnology.com>
2020-12-04 13:37:20 +0700

    BBSFR-47: Show total Product and add Sajari-react-hooks

--- components/Collection/Components/HeaderCollectionComponent.js
+++ components/Collection/Components/HeaderCollectionComponent.js
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react"
-import { Pipeline, Summary } from "@sajari/react-search-ui"
+import React, { useEffect, useState } from "react"
+import { Pipeline, Summary, SearchProvider } from "@sajari/react-search-ui"
 
 import { Container } from "react-bootstrap"
 import styles from "../Collections.module.scss"
+import { useSearch, Variables } from "@sajari/react-hooks"
 
 const HeaderCollectionComponent = ({ pageHeading }) => {
+  const [totalProduct, setTotalProduct] = useState(-1)
+  const [isSetTotal, setTotal] = useState(false)
   const pipeline = new Pipeline(
     {
       // account: "1606874199975641114",
@@ -17,11 +20,28 @@ const HeaderCollectionComponent = ({ pageHeading }) => {
     // "app"
     "query"
   )
+  const variables = new Variables({ q: "" })
+
+  const SearchPlayground = React.memo(() => {
+    const { totalResults } = useSearch({
+      variables,
+      pipeline,
+      fields: {},
+    })
+    totalResults != undefined && !isSetTotal && setTotalProduct(totalResults)
+    totalProduct != -1 && setTotal(true)
+    return (
+      <>
+        <div className={styles.product_count}>{`${totalProduct} Products`}</div>
+      </>
+    )
+  })
   return (
     <Container fluid className={styles.header}>
       <div className={styles.content}>
-        <div className={styles.page_heading_1}>awewe</div>
-        <div className={styles.product_count}>121321 product</div>
+        <div className={styles.page_heading_1}>{pageHeading}</div>
+        <div className={styles.product_count}></div>
+        <SearchPlayground />
       </div>
     </Container>
   )
--- components/Collection/index.js
+++ components/Collection/index.js
@@ -2,10 +2,13 @@ import ResultComponent from "./Components/ResultComponent"
 import HeaderCollectionComponent from "./Components/HeaderCollectionComponent"
 const isServer = () => typeof window === "undefined"
 const CollectionComponent = ({ collections }) => {
-  console.log(collections)
   return (
     <>
-      <HeaderCollectionComponent />
+      {!isServer() && (
+        <HeaderCollectionComponent
+          pageHeading={collections.page_heading_1[0].text}
+        />
+      )}
       {!isServer() && <ResultComponent />}
     </>
   )
