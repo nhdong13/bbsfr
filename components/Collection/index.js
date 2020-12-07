867286c219d1e69a6644e58fc50c40cf6597347f
LeNH NUS <lenh@nustechnology.com>
2020-12-07 10:59:23 +0700

    BBSFR-49: Show list Categories with condition

--- components/Collection/index.js
+++ components/Collection/index.js
@@ -1,18 +1,25 @@
 import ResultComponent from "./Components/ResultComponent"
 import HeaderCollectionComponent from "./Components/HeaderCollectionComponent"
+import CategoriesComponent from "./Components/CategoriesComponent"
 
 const isServer = () => typeof window === "undefined"
 const CollectionComponent = ({ collections }) => {
   return (
     <>
       {!isServer() && (
-        <HeaderCollectionComponent
-          pageHeading={collections.page_heading_1[0].text}
-        />
+        <div>
+          <HeaderCollectionComponent
+            pageHeading={collections.page_heading_1[0].text}
+          />
+          <CategoriesComponent
+            categories={collections.categories}
+            shopByCategoryText={collections.shop_by_category_text[0].text}
+          />
+          <ResultComponent />
+        </div>
       )}
-      {!isServer() && <ResultComponent />}
     </>
-  );
+  )
 }
 
 export default CollectionComponent
