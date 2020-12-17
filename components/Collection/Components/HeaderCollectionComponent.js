c1741543e9d88e8b5f093ca883a5b3bfd9445669
LeNH NUS <lenh@nustechnology.com>
2020-12-17 11:26:36 +0700

    BBSFR-51: Revert Header of Component

--- components/Collection/Components/HeaderCollectionComponent.js
+++ components/Collection/Components/HeaderCollectionComponent.js
@@ -1,12 +1,26 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import styles from "../Collections.module.scss";
-import { useSearchContext } from "@sajari/react-hooks";
+import React from "react"
+import { Container } from "react-bootstrap"
+import styles from "../Collections.module.scss"
+import { useSearchContext } from "@sajari/react-hooks"
 
 const HeaderCollectionComponent = ({ pageHeading }) => {
-  const { totalResults } = useSearchContext();
+  const { totalResults } = useSearchContext()
 
-  return <></>;
-};
+  return (
+    <>
+      <Container fluid className={styles.headerCollectionPage}>
+        <div className={styles.contentHeader}>
+          <div className={styles.page_heading_1_collection_page}>
+            {pageHeading}
+          </div>
+          <div className={styles.product_count_collection_page}></div>
+          <div className={styles.product_count_collection_page}>{`${
+            totalResults != undefined ? totalResults : 0
+          } Products`}</div>
+        </div>
+      </Container>
+    </>
+  )
+}
 
-export default HeaderCollectionComponent;
+export default HeaderCollectionComponent
