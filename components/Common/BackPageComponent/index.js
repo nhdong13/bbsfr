1bbefc905b86e7fe424bdd811e4bea5091ec5411
PhuocNH NUS <phuocnh@nustechnology.com>
2020-12-24 09:03:37 +0700

    Move to common and reuse component back to page before for collection and category page

---
+++ components/Common/BackPageComponent/index.js
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router"
+import { Container } from "react-bootstrap"
+import { capitalizeString } from "../../../services/collection"
+import styles from "../Common.module.scss"
+
+const BackToPageBeforeComponent = ({ page }) => {
+  console.log("Debug code page:", page)
+  const router = useRouter()
+  return (
+    <Container fluid className={styles.pre_page_button}>
+      <div onClick={() => router.back()}>&#8249; {capitalizeString(page)}</div>
+    </Container>
+  )
+}
+export default BackToPageBeforeComponent
