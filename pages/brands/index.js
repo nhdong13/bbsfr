20f59fd13756b0eefc7c3629dc17d5862ba6fc96
PhuocNH NUS <phuocnh@nustechnology.com>
2020-12-31 15:57:28 +0700

    Add func get brand data

--- pages/brands/index.js
+++ pages/brands/index.js
@@ -1,7 +1,11 @@
 import BrandComponent from "../../components/Brand"
+import { getDataBrandDirectory } from "../../lib/prismic/api"
+import { getDataForMainNav } from "../../services/mainNav"
 
 export async function getStaticProps() {
-  //Data brand_directory waiting the prismic data update
+  const dataNav = await getDataForMainNav()
+  const brandDirectory = await getDataBrandDirectory()
+  console.log("Debug code brandDirectory:", brandDirectory)
   return {
     props: {},
     revalidate: +process.env.NEXT_PUBLIC_REVALIDATE_PAGE_TIME,
