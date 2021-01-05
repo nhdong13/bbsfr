d0d38ea7a48edf07c1f25f56a90dc191f1985525
LeNH NUS <lenh@nustechnology.com>
2021-01-05 13:19:46 +0700

    asd

--- components/Brand/Components/BestSellersComponent.js
+++ components/Brand/Components/BestSellersComponent.js
@@ -3,38 +3,30 @@ import { useEffect, useState } from "react"
 import SessionBrowseByCategoryComponent from "../../DepartmentDetail/Sesstion/SessionBrowseByCategoryComponent"
 import { getDepartmentByUID } from "../../../lib/prismic/api"
 import styles from "../Brand.module.scss"
-import dynamic from "next/dynamic"
-import ImagedHeaderComponent from "./ImagedHeaderComponent"
-import {
-  useSearchContext,
-  useFilter,
-  SearchProvider,
-} from "@sajari/react-hooks"
-const SEODynamic = dynamic(() => import("../../HomePage/SEO"))
 
-const BrandHomeComponent = ({ element, heading }) => {
+const BrandHomeComponent = ({ element }) => {
   const [collections, setCollections] = useState([])
   useEffect(async () => {
     const { collections } = await getDepartmentByUID(element.department_slug)
     setCollections(collections)
   }, [element])
-  console.log(heading)
-
   return (
     <>
-      <ImagedHeaderComponent
-        header="abc"
-        productsCount="20"
-        imgUrl="https://news.itu.int/wp-content/uploads/2018/07/citymobility-min-e1530886118305.jpg"
-      />
+      <Container
+        fluid
+        className={styles.imagedHeader}
+        style={{
+          backgroundImage:
+            "url('https://news.itu.int/wp-content/uploads/2018/07/citymobility-min-e1530886118305.jpg')",
+        }}
+      >
+        <p className={styles.header}>ALPINESTARS</p>
+      </Container>
       <SessionBrowseByCategoryComponent
         departmentSlug={element.department_slug}
         collections={collections}
         disableTitleContainer={true}
       />
-      <img src="/1.png" width="100%" />
-      <img src="/2.png" width="100%" />
-      <SEODynamic heading1={"ALPINESTARS"} pageParagraph={[]} />
     </>
   )
 }
--- pages/brands/[home]/index.js
+++ pages/brands/[home]/index.js
@@ -1,6 +1,16 @@
+import { route } from "next/dist/next-server/server/router"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 import BrandHomeComponent from "../../../components/Brand/Components/BrandHomeComponent"
+import { getDepartmentByUID } from "../../../lib/prismic/api"
 
 const BrandHomePage = () => {
-  return <BrandHomeComponent />
+  const [element] = useState({
+    name: "Road Gear",
+    active: true,
+    department_slug: "road-gear",
+  })
+  const router = useRouter()
+  return <BrandHomeComponent element={element} heading={router?.query?.home} />
 }
 export default BrandHomePage
