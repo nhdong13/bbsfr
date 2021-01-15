b37378bcc01aab255d5276925024646e371c9ce8
Gavin NUS <gavin@nustechnology.com>
2021-01-15 15:13:58 +0700

    Add loading & handle error

--- components/Products/ProductDetails/index.js
+++ components/Products/ProductDetails/index.js
@@ -1,4 +1,7 @@
 import { useProductDetails } from "@sdk/react";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { Container, Row } from "react-bootstrap";
 import ProductImageCarousel from "../ProductImageCarousel";
 import ProductVariant from "../ProductVariant";
 import ProductShipping from "../ProductShipping";
@@ -7,23 +10,43 @@ import ProductDelivery from "../ProductDelivery";
 import ProductReturns from "../ProductReturns";
 import ProductCustomerReviews from "../ProductCustomerReviews";
 import AddToCart from "../AddToCart";
+import LoadingSpinner from "../../LoadingSpinner";
 
 
 function ProductDetailsComponent ({id}) {
+  const router = useRouter();
   const { loading, data, error } = useProductDetails({id: id})
 
   console.log(data)
+  useEffect(() => {
+    if (!loading && (error || !data)) {
+      router.push('/')
+    }
+  }, [data, loading])
+
   return(
-    <div className='product-details' >
-      <ProductImageCarousel />
-      <ProductVariant />
-      <ProductShipping />
-      <ProductDescription />
-      <ProductDelivery />
-      <ProductReturns />
-      <ProductCustomerReviews />
-      <AddToCart />
-    </div>
+    <>
+      {
+        loading &&
+        <LoadingSpinner show={loading}></LoadingSpinner>
+      }
+
+      { !loading && data &&
+        <Container fluid className='product-details' >
+          <Row>
+            <ProductImageCarousel />
+          </Row>
+          <ProductVariant />
+          <ProductShipping />
+          <ProductDescription />
+          <ProductDelivery />
+          <ProductReturns />
+          <ProductCustomerReviews />
+          <AddToCart />
+        </Container>
+      }
+    </>
+
   )
 }
 
