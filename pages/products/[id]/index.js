238fc7c5ff1f21e7ca880bdf0c329f6d18143a6e
Gavin NUS <gavin@nustechnology.com>
2021-01-18 16:40:02 +0700

    Render product at server side

--- pages/products/[id]/index.js
+++ pages/products/[id]/index.js
@@ -1,15 +1,48 @@
 import { useRouter } from 'next/router'
+import { productDetails } from "lib/@sdk/queries/products";
+import { initializeApollo } from "lib/apollo";
+
 
 import ProductDetailsComponent from "../../../components/Products/ProductDetails";
 
-function ProductDetails() {
+export async function getStaticProps({ params }) {
+  const apolloClient = initializeApollo();
+  const response = await apolloClient.query({
+    query: productDetails,
+    variables: {id: params.id}
+  });
+
+  const { data, loading } = response;  
+  return {
+    props: { 
+      product: data.product,
+      loading: loading,
+      initialApolloState: apolloClient.cache.extract()
+    },
+    revalidate: 1,
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: '*' } }
+    ],
+    fallback: 'blocking'
+  };
+}
+
+function ProductDetails({product, loading}) {
   const router = useRouter();
   const { id } = router.query;
   return(
     <>
       {
         id &&
-        <ProductDetailsComponent id={id}/>
+        <ProductDetailsComponent
+          loading={loading}
+          product={product}
+        />
       }
     </>
   )
