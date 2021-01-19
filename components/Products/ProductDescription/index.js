01f4f1a87c717295be972d4b894c7d585ef42a43
Gavin NUS <gavin@nustechnology.com>
2021-01-19 15:15:46 +0700

    Fixes formater

--- components/Products/ProductDescription/index.js
+++ components/Products/ProductDescription/index.js
@@ -1,23 +1,20 @@
-import ReadMore from "../../ReadMore"
+import ReadMore from "../../ReadMore";
 import styles from "./ProductDescription.module.scss";
 
 function ProductDescription({ description }) {
   const readMoreSettings = {
     charLimit: 450,
-    readMoreText: 'Read more',
-    readLessText: 'Read less',
-    className: styles.readMoreContent
-  }
-  return(
+    readMoreText: "Read more",
+    readLessText: "Read less",
+    className: styles.readMoreContent,
+  };
+
+  return (
     <div className={styles.productDetailsDescription}>
       <div className={styles.title}>Product Details</div>
-      <ReadMore
-        settings={readMoreSettings}
-      >
-        {description}
-      </ReadMore>
+      <ReadMore settings={readMoreSettings}>{description}</ReadMore>
     </div>
-  )
+  );
 }
 
 export default ProductDescription;
--- components/ReadMore/index.js
+++ components/ReadMore/index.js
@@ -1,69 +1,67 @@
 import React, { useState } from "react";
 import styles from "./ReadMore.module.scss";
 
-function ReadMore ({ children, settings }) {
+function ReadMore({ children, settings }) {
   const defaultSettings = {
     readMoreText: "Read more",
     readLessText: "Read less",
     readMoreColor: "#007bff",
-    charLimit: 450
-  }
+    charLimit: 450,
+  };
 
   const appSettings = Object.assign(defaultSettings, settings);
-  const [charLimit, setCharLimit] = useState(appSettings.charLimit)
+  const [charLimit, setCharLimit] = useState(appSettings.charLimit);
 
   const getReadMoreContent = () => {
     const { readMoreText, readLessText, className } = appSettings;
 
     if (children.length > charLimit) {
-      return (<div className={`short-text ${className}`}>
-        {children.substr(0, charLimit)}...
-        <div className={styles.gradientBackground}></div>
-        <div
-          className="readMoreText"
-          style={{ color: '#007bff', cursor: 'pointer' }}
-          role="presentation"
-          dangerouslySetInnerHTML={{__html: readMoreText}}
-          onClick={() => showLongText()}
-        >
+      return (
+        <div className={`short-text ${className}`}>
+          {children.substr(0, charLimit)}...
+          <div className={styles.gradientBackground}></div>
+          <div
+            className="readMoreText"
+            style={{ color: "#007bff", cursor: "pointer" }}
+            role="presentation"
+            dangerouslySetInnerHTML={{ __html: readMoreText }}
+            onClick={() => showLongText()}
+          ></div>
         </div>
-      </div>);
+      );
     } else if (children.length < charLimit) {
       return (
-        <div 
+        <div
           className={`short-text ${className}`}
-          dangerouslySetInnerHTML={{__html: children}}
-        >
-      </div>);
+          dangerouslySetInnerHTML={{ __html: children }}
+        ></div>
+      );
     }
-    return (<div className={`short-text ${className}`}>
-      {children}
-      <div
-        className="readMoreText"
-        style={{ color: '#007bff', cursor: 'pointer' }}
-        role="presentation"
-        dangerouslySetInnerHTML={{__html: readLessText}}
-        onClick={() => showShortText()}
-      >
+    return (
+      <div className={`short-text ${className}`}>
+        {children}
+        <div
+          className="readMoreText"
+          style={{ color: "#007bff", cursor: "pointer" }}
+          role="presentation"
+          dangerouslySetInnerHTML={{ __html: readLessText }}
+          onClick={() => showShortText()}
+        ></div>
       </div>
-    </div>);
+    );
   };
 
   const showLongText = () => {
-    setCharLimit(children.length)
+    setCharLimit(children.length);
     getReadMoreContent();
-  }
+  };
 
   const showShortText = () => {
-    setCharLimit(appSettings.charLimit)
+    setCharLimit(appSettings.charLimit);
     getReadMoreContent();
-  }
+  };
 
-  return (
-    <div className={styles.readMoreContent}>
-      {getReadMoreContent()}
-    </div>
-  )
+  return <div className={styles.readMoreContent}>{getReadMoreContent()}</div>;
 }
 
 export default ReadMore;
