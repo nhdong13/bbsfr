de7f30844fc3cddfde7aceae6aaf60580c73a82a
LeNH NUS <lenh@nustechnology.com>
2021-01-22 15:58:23 +0700

    Hotfix/ Remove '/' when generate path

--- pages/vehicles/[vehicle]/[vehicleCollection]/index.js
+++ pages/vehicles/[vehicle]/[vehicleCollection]/index.js
@@ -1,45 +1,45 @@
-import { search } from "@sajari/server"
-import VehicleCollectionComponent from "../../../../components/Vehicles/VehicleCollection"
-import { getVehicleCollectionByUid } from "../../../../lib/prismic/api"
-import { pipelineConfig, variablesConfig } from "../../../../lib/sajari/config"
+import { search } from "@sajari/server";
+import VehicleCollectionComponent from "../../../../components/Vehicles/VehicleCollection";
+import { getVehicleCollectionByUid } from "../../../../lib/prismic/api";
+import { pipelineConfig, variablesConfig } from "../../../../lib/sajari/config";
 import {
   categoryFilter,
   colorFilter,
   listBrandsFilter,
   priceRangeFilter,
   ratingFilter,
-} from "../../../../lib/sajari/filter"
-import { getDataForMainNav } from "../../../../services/mainNav"
-import { authenticationFromStamped } from "../../../../services/testimonial"
-import { SSRProvider, SearchProvider } from "@sajari/react-search-ui"
-import { listVehicleService } from "../../../../services/vehicle"
+} from "../../../../lib/sajari/filter";
+import { getDataForMainNav } from "../../../../services/mainNav";
+import { authenticationFromStamped } from "../../../../services/testimonial";
+import { SSRProvider, SearchProvider } from "@sajari/react-search-ui";
+import { listVehicleService } from "../../../../services/vehicle";
 
 export async function getStaticPaths() {
-  const paths = []
-  const vehicleList = await listVehicleService()
+  const paths = [];
+  const vehicleList = await listVehicleService();
   for (const vehicle of vehicleList || []) {
-    let vehicleCollections = vehicle?.node?.collections || []
+    let vehicleCollections = vehicle?.node?.collections || [];
     for (const collection of vehicleCollections) {
       if (collection?.collection_slug) {
         paths.push(
-          `/vehicles/${vehicle.node._meta.uid}/${collection.collection_slug}`
-        )
+          `/vehicles/${vehicle.node._meta.uid}${collection.collection_slug}`
+        );
       }
     }
   }
-  return { paths, fallback: false }
+  return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
-  const dataNav = await getDataForMainNav()
+  const dataNav = await getDataForMainNav();
   const resStamped = await fetch(
     process.env.STAMPED_API_URL,
     authenticationFromStamped()
-  )
-  const testimonials = await resStamped.json()
+  );
+  const testimonials = await resStamped.json();
   const vehicleCollection = await getVehicleCollectionByUid(
     params?.vehicleCollection
-  )
+  );
   //Filter options will replace base params for per page --> this is code demo
   const initialResponse = await search({
     pipeline: pipelineConfig,
@@ -51,11 +51,11 @@ export async function getStaticProps({ params }) {
       ratingFilter,
       colorFilter,
     ],
-  })
+  });
   return {
     props: { dataNav, vehicleCollection, initialResponse, testimonials },
     revalidate: +process.env.NEXT_PUBLIC_REVALIDATE_PAGE_TIME,
-  }
+  };
 }
 
 const VehicleCollectionPage = ({
@@ -98,6 +98,6 @@ const VehicleCollectionPage = ({
         />
       </SearchProvider>
     </SSRProvider>
-  )
-}
-export default VehicleCollectionPage
+  );
+};
+export default VehicleCollectionPage;
